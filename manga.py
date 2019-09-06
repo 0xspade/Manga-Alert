88a35287bbfe62b911879cf3883e9f1763828679
Spade <18681023+phspade@users.noreply.github.com>
2019-09-06 10:36:29 +0000

    Update manga.py

--- manga.py
+++ manga.py
@@ -2,31 +2,35 @@
 import datetime, cfscrape, requests
 
 x = datetime.datetime.now()
-key="" ## Create from BotFather in Telegram
-id="" ## Your Telegram ID
+key="" ## Create your own bot api_key to BotFather in Telegram
+id="" ## Find your Telegram ID >> google is your friend
 
 current_date = x.strftime("%Y/%m/%d")
 manga=[["One Piece","https://w10.mangafreak.net/Manga/One_Piece"], ["One Punch Man","https://w10.mangafreak.net/Manga/Onepunch_Man"]]
 
+def string(text):
+    return str(b'%s', encoding='utf-8', errors='strict') %format(text)
+
 def alert(manga_url, date):
     scraper = cfscrape.create_scraper()
     request = scraper.get(manga_url)
-    date = str(b'%s', encoding='utf-8', errors='strict') %format(date)
-    req = str(b'%s', encoding='utf-8', errors='strict') %format(request.content)
+    date = string(date)
+    req = string(request.content)
     datex = "<tr>"+date+"</tr>"
     if datex in req:
         return True
     else:
         return False
 
-def send_alert(id, api_key, message):
-	api_key = str(b'%s', encoding='utf-8', errors='strict') %format(api_key)
-	id = str(b'%s', encoding='utf-8', errors='strict') %format(id)
-	message = str(b'%s', encoding='utf-8', errors='strict') %format(message)
 
-	url="https://api.telegram.org/bot"+api_key+"/sendmessage"
-	data={"chat_id":id,"text":message}
-	requests.get(url, data=data)
+def send_alert(id, api_key, message):
+    api_key = string(api_key)
+    id = string(id)
+    message = string(message)
+
+    url="https://api.telegram.org/bot"+api_key+"/sendmessage"
+    data={"chat_id":id,"text":message}
+    requests.get(url, data=data)
 
 for manga_url in manga:
     title=manga_url[0]
